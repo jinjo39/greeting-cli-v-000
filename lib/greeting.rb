e3ddbec106e5cd8667f71439748ae89b0b6322e0
Christina Delavar <c.delavar@gmail.com>
2018-08-15 18:16:04 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,6 @@
-# code the #greeting method here!
+#code the #greeting method here!
+def greeting(name)
+  puts "What is your name?"
+  name = gets.strip
+  puts "Hello #{name}! It's nice to meet you."
+end
